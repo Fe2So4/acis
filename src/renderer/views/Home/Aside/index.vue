7a6c8e01473928b25f5df837bd99946dbd6eaab0
dyw-design <1875742887@qq.com>
2020-05-26 19:14:03 +0800

    新增头部侧边栏文件夹

---
+++ src/renderer/views/Home/Aside/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="aside">
+        aside123456
+    </div>
+</template>
+<script>
+export default {
+  name: 'Aside',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+    .aside{
+
+    }
+</style>
--- src/renderer/views/Home/index.vue
+++ src/renderer/views/Home/index.vue
@@ -1,48 +1,61 @@
 <template>
-    <div class="home">
-       <el-container>
-        <el-header height="100px">Header</el-header>
-        <el-container>
-            <el-aside width="200px">Aside</el-aside>
-            <el-main>Main</el-main>
-        </el-container>
-        <el-footer height="20px">foot</el-footer>
+  <div class="home">
+    <el-container>
+      <el-header height="100px">
+        <Header></Header>
+      </el-header>
+      <el-container>
+        <el-aside width="200px">
+          <Aside></Aside>
+        </el-aside>
+        <el-main>Main</el-main>
+      </el-container>
+      <el-footer height="20px">foot</el-footer>
     </el-container>
-    </div>
+  </div>
 </template>
 <script>
-    export default {
-      data () { return {} }
+import Header from './Header/index'
+import Aside from './Aside/index'
+export default {
+  name: 'home',
+  data () {
+    return {
+
     }
+  },
+  components: {
+    Header, Aside
+  }
+}
 </script>  
 <style lang="scss" scoped>
-.home{
-    height:100%;
-      .el-header, .el-footer {
-    background-color: #B3C0D1;
+.home {
+  height: 100%;
+  .el-header,
+  .el-footer {
+    background-color: #b3c0d1;
     color: #333;
   }
-  .el-container{
-      height: 100%;
-      .el-container{
-        height: calc(100% - 120px);
-        display: flex;
-      }
-   
-  } 
+  .el-container {
+    height: 100%;
+    .el-container {
+      height: calc(100% - 120px);
+      display: flex;
+    }
+  }
   .el-aside {
-    background-color: #D3DCE6;
+    background-color: #d3dce6;
     color: #333;
     text-align: center;
     line-height: 200px;
   }
-   .el-main {
+  .el-main {
     flex: 1;
-    background-color: #E9EEF3;
+    background-color: #e9eef3;
     color: #333;
     text-align: center;
     height: calc(100%);
   }
 }
-
 </style>    
