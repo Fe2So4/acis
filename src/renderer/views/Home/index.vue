c451500c6c8f2c8b6c9b9ee6db91e5ef6f75d4c7
dyw-design <1875742887@qq.com>
2020-05-26 18:45:18 +0800

    首页基本布局

--- src/renderer/views/Home/index.vue
+++ src/renderer/views/Home/index.vue
@@ -1,6 +1,13 @@
 <template>
     <div class="home">
-        home
+       <el-container>
+        <el-header height="100px">Header</el-header>
+        <el-container>
+            <el-aside width="200px">Aside</el-aside>
+            <el-main>Main</el-main>
+        </el-container>
+        <el-footer height="20px">foot</el-footer>
+    </el-container>
     </div>
 </template>
 <script>
@@ -9,5 +16,33 @@
     }
 </script>  
 <style lang="scss" scoped>
+.home{
+    height:100%;
+      .el-header, .el-footer {
+    background-color: #B3C0D1;
+    color: #333;
+  }
+  .el-container{
+      height: 100%;
+      .el-container{
+        height: calc(100% - 120px);
+        display: flex;
+      }
+   
+  } 
+  .el-aside {
+    background-color: #D3DCE6;
+    color: #333;
+    text-align: center;
+    line-height: 200px;
+  }
+   .el-main {
+    flex: 1;
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+    height: calc(100%);
+  }
+}
 
 </style>    
