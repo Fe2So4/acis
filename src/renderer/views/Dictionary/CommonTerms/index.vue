1d3f75a52112e6768bf96519e929f5b6218043aa
dyw-design <1875742887@qq.com>
2020-07-24 09:12:18 +0800

    fx

--- src/renderer/views/Dictionary/CommonTerms/index.vue
+++ src/renderer/views/Dictionary/CommonTerms/index.vue
@@ -45,19 +45,11 @@
             template(v-slot:edit="{ row }")
               el-input(v-model="row.detailCode" size="mini" @blur="handleBlur")
     .option
-<<<<<<< HEAD
-      el-button(size="mini" :disabled="optionType!==1" @click="insertEvent(-1)") 新增(N)
-      el-button(size="mini" @click="$refs.xTable.removeCheckboxRow()" :disabled="optionType===1") 删除(D)
-      el-button(size="mini" @click="saveEvent" :disabled="optionType===1") 保存(S)
-      el-button(size="mini" @click="revertEvent" :disabled="optionType===1") 取消(C)
-      el-button(size="mini") 刷新(R)
-=======
       el-button(size="mini" :disabled="addDisabled" @click="insertEvent(-1)") 新增(N)
       el-button(size="mini" :disabled="deleteDisabled" @click="$refs.xTable.removeCheckboxRow()") 删除(D)
       el-button(size="mini" :disabled="saveDisabled" @click="saveEvent") 保存(S)
       el-button(size="mini" :disabled="cancelDisabled" @click="revertEvent") 取消(C)
       el-button(size="mini" :disabled="refreshDisabled") 刷新(R)
->>>>>>> c24ccfd1e6614ae687d792facc8cc46ea94d5405
 </template>
 <script>
 import {
@@ -65,28 +57,23 @@ import {
   commonTermsDetail,
   addCommonTermsDetail,
   updateCommonTermsDetail
-} from "@/api/dictionary";
-import request from "@/utils/requestForMock";
+} from '@/api/dictionary'
+import request from '@/utils/requestForMock'
 export default {
-  data() {
+  data () {
     return {
       wrapStyle: [
         {
-          "overflow-x": "hidden"
+          'overflow-x': 'hidden'
         }
       ],
       tableData: [],
       data: [],
       defaultProps: {
-        children: "children",
-        label: "itemName"
+        children: 'children',
+        label: 'itemName'
       },
       currentMenu: {},
-<<<<<<< HEAD
-      optionType: 1 // 新增状态,
-      // currentRow: {}
-    }
-=======
       optionType: 1, // 新增状态
       addDisabled: false,
       deleteDisabled: true,
@@ -94,13 +81,12 @@ export default {
       cancelDisabled: true,
       refreshDisabled: true,
       insertData: {}
-    };
->>>>>>> c24ccfd1e6614ae687d792facc8cc46ea94d5405
+    }
   },
   watch: {
     data: {
-      handler(val) {
-        return val;
+      handler (val) {
+        return val
       }
     }
     // insertData: {
@@ -119,81 +105,75 @@ export default {
     //   deep: true
     // }
   },
-  mounted() {
-    this.getCommonTerms();
+  mounted () {
+    this.getCommonTerms()
   },
   methods: {
-<<<<<<< HEAD
-    currentChangeEvent ({ row }) {
-      console.log(row)
-      // this.currentRow = row
-=======
-    handleBlur() {
-      const { insertRecords, updateRecords } = this.$refs.xTable.getRecordset();
+    handleBlur () {
+      const { insertRecords } = this.$refs.xTable.getRecordset()
       if (insertRecords.length > 0) {
         if (
-          insertRecords[0].detailCode !== "" ||
-          insertRecords[0].detailName !== ""
+          insertRecords[0].detailCode !== '' ||
+          insertRecords[0].detailName !== ''
         ) {
-          this.saveDisabled = false;
-          this.cancelDisabled = false;
-          this.deleteDisabled = false;
+          this.saveDisabled = false
+          this.cancelDisabled = false
+          this.deleteDisabled = false
         } else {
-          this.saveDisabled = true;
-          this.cancelDisabled = false;
-          this.deleteDisabled = true;
+          this.saveDisabled = true
+          this.cancelDisabled = false
+          this.deleteDisabled = true
         }
       }
     },
-    currentChangeEvent({ row }) {
-      console.log(row);
-      this.insertData = row;
-      if (row.detailCode === "" && row.detailName === "") {
-        this.deleteDisabled = true;
-        this.cancelDisabled = false;
+    currentChangeEvent ({ row }) {
+      console.log(row)
+      this.insertData = row
+      if (row.detailCode === '' && row.detailName === '') {
+        this.deleteDisabled = true
+        this.cancelDisabled = false
       } else {
-        this.deleteDisabled = false;
+        this.deleteDisabled = false
       }
->>>>>>> c24ccfd1e6614ae687d792facc8cc46ea94d5405
     },
-    handleChange(val) {
-      this.currentMenu = val;
-      this.getCommonTermsDetail();
+    handleChange (val) {
+      this.currentMenu = val
+      this.getCommonTermsDetail()
     },
-    getCommonTerms() {
+    getCommonTerms () {
       request({
-        method: "GET",
+        method: 'GET',
         url: commonTermsList
       }).then(res => {
-        const data = res.data.data;
-        this.data = data;
-      });
+        const data = res.data.data
+        this.data = data
+      })
     },
-    getCommonTermsDetail() {
+    getCommonTermsDetail () {
       request({
-        method: "GET",
-        url: commonTermsDetail + "/" + this.currentMenu.itemCode
+        method: 'GET',
+        url: commonTermsDetail + '/' + this.currentMenu.itemCode
       }).then(res => {
-        const data = res.data.data;
-        this.tableData = data;
-      });
+        const data = res.data.data
+        this.tableData = data
+      })
     },
-    async insertEvent(row) {
+    async insertEvent (row) {
       // this.optionType = 2;
-      this.addDisabled = true;
-      this.cancelDisabled = false;
+      this.addDisabled = true
+      this.cancelDisabled = false
       this.data.forEach(value => {
-        value.disabled = true;
-      });
+        value.disabled = true
+      })
       const record = {
         detailId: this.tableData.length + 1,
         itemName: this.currentMenu.itemName,
-        detailCode: "",
-        detailName: ""
-      };
-      this.insertData = record;
-      const { row: newRow } = await this.$refs.xTable.insertAt(record, row);
-      console.log(newRow);
+        detailCode: '',
+        detailName: ''
+      }
+      this.insertData = record
+      const { row: newRow } = await this.$refs.xTable.insertAt(record, row)
+      console.log(newRow)
       // await this.$refs.xTable.setActiveCell(newRow, 'sex')
     },
     // rowDrop () {
@@ -208,72 +188,63 @@ export default {
     //     })
     //   })
     // },
-    revertEvent() {
-      this.$refs.xTable.revertData();
-      this.cancelDisabled = true;
-      this.addDisabled = false;
+    revertEvent () {
+      this.$refs.xTable.revertData()
+      this.cancelDisabled = true
+      this.addDisabled = false
     },
-<<<<<<< HEAD
     addCommonTermsDetail (param) {
-      const obj = {}
-      obj.detailId = param.detailId
-      obj.detailCode = param.detailCode
-      obj.detailName = param.detailName
-      obj.itemCode = this.currentMenu.itemCode
-=======
-    addCommonTermsDetail(param) {
       // "detailCode": "",
       // "detailId": "",
       // "detailName": "",
       // "itemCode": "",
       // "sort": 0,
       // "state": true
-      const obj = {};
-      obj.detailId = param.detailId;
-      obj.detailCode = param.detailCode;
-      obj.detailName = param.detailName;
-      obj.itemCode = this.currentMenu.itemCode;
->>>>>>> c24ccfd1e6614ae687d792facc8cc46ea94d5405
+      const obj = {}
+      obj.detailId = param.detailId
+      obj.detailCode = param.detailCode
+      obj.detailName = param.detailName
+      obj.itemCode = this.currentMenu.itemCode
       request({
-        method: "POST",
+        method: 'POST',
         url: addCommonTermsDetail,
         data: obj
-      }).then(res => this.getCommonTermsDetail());
+      }).then(res => this.getCommonTermsDetail())
     },
-    updateCommonTermsDetail(param) {
-      const list = param;
+    updateCommonTermsDetail (param) {
+      const list = param
       list.forEach(value => {
-        value.itemCode = this.currentMenu.itemCode;
+        value.itemCode = this.currentMenu.itemCode
         for (var k in value) {
-          if (k === "itemName") {
-            delete value[k];
+          if (k === 'itemName') {
+            delete value[k]
           }
         }
-      });
+      })
       request({
-        method: "PUT",
+        method: 'PUT',
         url: updateCommonTermsDetail,
         data: list
-      }).then(res => this.getCommonTermsDetail());
+      }).then(res => this.getCommonTermsDetail())
     },
-    saveEvent() {
-      const { insertRecords, updateRecords } = this.$refs.xTable.getRecordset();
+    saveEvent () {
+      const { insertRecords, updateRecords } = this.$refs.xTable.getRecordset()
       // this.$XModal.alert(`insertRecords=${insertRecords.length} removeRecords=${removeRecords.length} updateRecords=${updateRecords.length}`)
-      console.log(insertRecords, updateRecords);
+      console.log(insertRecords, updateRecords)
       if (insertRecords.length > 0) {
-        this.addCommonTermsDetail(insertRecords[0]);
+        this.addCommonTermsDetail(insertRecords[0])
       }
       if (updateRecords.length > 0) {
-        this.updateCommonTermsDetail(updateRecords);
+        this.updateCommonTermsDetail(updateRecords)
       }
-      this.addDisabled = false;
-      this.saveDisabled = true;
-      this.refreshDisabled = false;
-      this.cancelDisabled = true;
-      this.deleteDisabled = true;
+      this.addDisabled = false
+      this.saveDisabled = true
+      this.refreshDisabled = false
+      this.cancelDisabled = true
+      this.deleteDisabled = true
     }
   }
-};
+}
 </script>
 <style lang="stylus" scoped>
 .common-terms {
