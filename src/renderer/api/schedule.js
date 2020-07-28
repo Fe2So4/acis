42e588b932897bccb87f44d446173df698323c3f
dyw-design <1875742887@qq.com>
2020-07-28 08:54:43 +0800

    格式化合并提交

--- src/renderer/api/schedule.js
+++ src/renderer/api/schedule.js
@@ -13,25 +13,25 @@ export const getNurseList = `${config.default.api.baseURL}/acis/operation/getOpe
 export const distributeOpeApply = `${config.default.api.baseURL}/acis/operation/arrangeOpeRoom`
 // 取消手术申请
 export const cancelOpeApply = `${config.default.api.baseURL}/acis/operation/deleteRoom`
-//安排主麻并生成日志
+// 安排主麻并生成日志
 export const distributeMainAnaes = `${config.default.api.baseURL}/acis/operation/arrangeFirstAnesDoc`
-//安排洗手护士并生成日志
+// 安排洗手护士并生成日志
 export const distributeWashNurse = `${config.default.api.baseURL}/acis/operation/arrangeOpeNurse`
-//安排副麻并生成日志
+// 安排副麻并生成日志
 export const distributeSubAnaes = `${config.default.api.baseURL}/acis/operation/arrangeOtherAnesDoc`
-//安排巡回护士并生成日志
+// 安排巡回护士并生成日志
 export const distributeHangNurse = `${config.default.api.baseURL}/acis/operation/arrangeSupplyNurse`
-//交换两个手术间数据
+// 交换两个手术间数据
 export const changeRoomData = `${config.default.api.baseURL}/acis/operation/changeDataFromTwoRooms`
-//更换手术间信息
+// 更换手术间信息
 export const changeTwoRoomData = `${config.default.api.baseURL}/acis/operation/changeRoom`
-//清空手术间
+// 清空手术间
 export const clearAllRoomData = `${config.default.api.baseURL}/acis/operation/clearArrange`
-//撤销手术间或麻醉医生护士安排
+// 撤销手术间或麻醉医生护士安排
 export const cancelClearDistribute = `${config.default.api.baseURL}/acis/operation/deleteLog`
-//撤销清空操作
+// 撤销清空操作
 export const cancelClearOption = `${config.default.api.baseURL}/acis/operation/deleteLogBySysno`
-//通过手术间号更改手术间配置信息
+// 通过手术间号更改手术间配置信息
 export const changeConfig = `${config.default.api.baseURL}/acis/operation/editRoomDetailByRoomNo`
-//麻醉方法查询
+// 麻醉方法查询
 export const getAnaesMethod = `${config.default.api.baseURL}/acis/operation/getAnesMethodList`
--- src/renderer/views/HomeNew/Aside/index.vue
+++ src/renderer/views/HomeNew/Aside/index.vue
@@ -1,17 +1,42 @@
 <template>
   <div class="aside">
-    <div class="title">Dandelion 麻醉临床信息系统</div>
-    <div class="img" @click="jumpLogin">
-      <img src="../../../assets/tq.png" alt />
+    <div class="title">
+      Dandelion 麻醉临床信息系统
+    </div>
+    <div
+      class="img"
+      @click="jumpLogin"
+    >
+      <img
+        src="../../../assets/tq.png"
+        alt
+      >
     </div>
     <div class="nav-list">
-      <el-scrollbar style="height:100%;" class="scrollbar">
-        <el-collapse accordion :value="activesNames" @change="handleChange">
-          <el-collapse-item v-for="item in navList" :key="item.index" :name="item.index">
+      <el-scrollbar
+        style="height:100%;"
+        class="scrollbar"
+      >
+        <el-collapse
+          accordion
+          :value="activesNames"
+          @change="handleChange"
+        >
+          <el-collapse-item
+            v-for="item in navList"
+            :key="item.index"
+            :name="item.index"
+          >
             <template slot="title">
-              <i class="header-icon" :class="item.icon" />
+              <i
+                class="header-icon"
+                :class="item.icon"
+              />
               {{ item.name }}
-              <div class="active" v-show="item.index === activesNames" />
+              <div
+                class="active"
+                v-show="item.index === activesNames"
+              />
             </template>
             <div class="menu">
               <span
@@ -35,160 +60,160 @@
   </div>
 </template>
 <script>
-import Dialog from "@/components/DialogNav/index";
-import { mapActions, mapGetters } from "vuex";
+import Dialog from '@/components/DialogNav/index'
+import { mapActions, mapGetters } from 'vuex'
 // import Overview from '../../../components/OperationOverview/index'
 export default {
-  name: "Aside",
-  data() {
+  name: 'Aside',
+  data () {
     return {
-      dialogTitle: "标题",
-      activesNames: "1",
+      dialogTitle: '标题',
+      activesNames: '1',
       isCollapse: false,
       showOverflow: false,
       showDialog: false, // 开启弹窗
-      componentName: "",
+      componentName: '',
       navList: [
         {
-          name: "大事件",
-          index: "1",
+          name: '大事件',
+          index: '1',
           subNav: [
-            { name: "麻药", index: "1-1", componentName: "Event" },
-            { name: "用药", index: "1-2", componentName: "Event" },
-            { name: "事件", index: "1-3", componentName: "Event" },
-            { name: "输液", index: "1-4", componentName: "Event" },
-            { name: "出量", index: "1-5", componentName: "Event" },
-            { name: "插管", index: "1-6", componentName: "Event" },
-            { name: "拔管", index: "1-7", componentName: "Event" },
-            { name: "输血", index: "1-8", componentName: "Event" },
-            { name: "输氧", index: "1-9", componentName: "Event" },
-            { name: "呼吸", index: "1-10", componentName: "Event" },
-            { name: "其他", index: "1-11", componentName: "Event" }
+            { name: '麻药', index: '1-1', componentName: 'Event' },
+            { name: '用药', index: '1-2', componentName: 'Event' },
+            { name: '事件', index: '1-3', componentName: 'Event' },
+            { name: '输液', index: '1-4', componentName: 'Event' },
+            { name: '出量', index: '1-5', componentName: 'Event' },
+            { name: '插管', index: '1-6', componentName: 'Event' },
+            { name: '拔管', index: '1-7', componentName: 'Event' },
+            { name: '输血', index: '1-8', componentName: 'Event' },
+            { name: '输氧', index: '1-9', componentName: 'Event' },
+            { name: '呼吸', index: '1-10', componentName: 'Event' },
+            { name: '其他', index: '1-11', componentName: 'Event' }
           ],
-          icon: "el-icon-star-on"
+          icon: 'el-icon-star-on'
         },
-        { name: "系统集成", index: "2", subNav: [], icon: "el-icon-location" },
+        { name: '系统集成', index: '2', subNav: [], icon: 'el-icon-location' },
         {
-          name: "患者操作",
-          index: "3",
+          name: '患者操作',
+          index: '3',
           subNav: [
             {
-              name: "手术概览",
-              index: "3-1",
-              componentName: "OperationOverview"
+              name: '手术概览',
+              index: '3-1',
+              componentName: 'OperationOverview'
             },
             {
-              name: "术中患者列表",
-              index: "3-2",
-              componentName: "IntraoperativePatients"
+              name: '术中患者列表',
+              index: '3-2',
+              componentName: 'IntraoperativePatients'
             },
             {
-              name: "术中登记",
-              index: "3-3",
-              componentName: "IntraoperativeRegistration"
+              name: '术中登记',
+              index: '3-3',
+              componentName: 'IntraoperativeRegistration'
             },
-            { name: "患者详情", index: "3-4", componentName: "PatientDetail" },
+            { name: '患者详情', index: '3-4', componentName: 'PatientDetail' },
             {
-              name: "手术取消",
-              index: "3-5",
-              componentName: "OperationCancel"
-            },
-            {
-              name: "复苏进程",
-              index: "3-6",
-              componentName: "OperationCancel"
+              name: '手术取消',
+              index: '3-5',
+              componentName: 'OperationCancel'
             },
             {
-              name: "复苏床位",
-              index: "3-7",
-              componentName: "ResuscitationBed"
-            },
-            { name: "实时状态", index: "3-8", componentName: "RealtimeStatus" },
-            { name: "麻醉评分", index: "3-9", componentName: "AnaesScore" },
-            {
-              name: "检验信息",
-              index: "3-10",
-              componentName: "InspectionInformation"
+              name: '复苏进程',
+              index: '3-6',
+              componentName: 'OperationCancel'
             },
             {
-              name: "医嘱信息",
-              index: "3-11",
-              componentName: "MedicalOrderInformation"
+              name: '复苏床位',
+              index: '3-7',
+              componentName: 'ResuscitationBed'
+            },
+            { name: '实时状态', index: '3-8', componentName: 'RealtimeStatus' },
+            { name: '麻醉评分', index: '3-9', componentName: 'AnaesScore' },
+            {
+              name: '检验信息',
+              index: '3-10',
+              componentName: 'InspectionInformation'
             },
             {
-              name: "检查结果",
-              index: "3-12",
-              componentName: "InspectionResult"
+              name: '医嘱信息',
+              index: '3-11',
+              componentName: 'MedicalOrderInformation'
             },
             {
-              name: "病历病程",
-              index: "3-13",
-              componentName: "MedicalRecordCourse"
-            },
-            { name: "状态维护", index: "3-14", componentName: "StateMaintain" },
-            {
-              name: "手术等级",
-              index: "3-15",
-              componentName: "OperationLevel"
+              name: '检查结果',
+              index: '3-12',
+              componentName: 'InspectionResult'
             },
             {
-              name: "术后登记",
-              index: "3-16",
-              componentName: "PostoperativeRegistration"
+              name: '病历病程',
+              index: '3-13',
+              componentName: 'MedicalRecordCourse'
+            },
+            { name: '状态维护', index: '3-14', componentName: 'StateMaintain' },
+            {
+              name: '手术等级',
+              index: '3-15',
+              componentName: 'OperationLevel'
+            },
+            {
+              name: '术后登记',
+              index: '3-16',
+              componentName: 'PostoperativeRegistration'
             }
           ],
-          icon: "el-icon-s-data"
+          icon: 'el-icon-s-data'
         },
         {
-          name: "常用功能",
-          index: "4",
+          name: '常用功能',
+          index: '4',
           subNav: [
-            { name: "血流动力", index: "4-1", componentName: "Hemodynamics" },
+            { name: '血流动力', index: '4-1', componentName: 'Hemodynamics' },
             {
-              name: "模板管理",
-              index: "4-2",
-              componentName: "TemplateManagement"
+              name: '模板管理',
+              index: '4-2',
+              componentName: 'TemplateManagement'
             },
             {
-              name: "手术交班",
-              componentName: "OperationHandover",
-              index: "4-3"
+              name: '手术交班',
+              componentName: 'OperationHandover',
+              index: '4-3'
             },
-            { name: "PACU状态", componentName: "PacuStatus", index: "4-4" },
-            { name: "设备采集", componentName: "DeviceGather", index: "4-5" },
-            { name: "字典", componentName: "Dictionary", index: "4-6" }
+            { name: 'PACU状态', componentName: 'PacuStatus', index: '4-4' },
+            { name: '设备采集', componentName: 'DeviceGather', index: '4-5' },
+            { name: '字典', componentName: 'Dictionary', index: '4-6' }
           ],
-          icon: "el-icon-monitor"
+          icon: 'el-icon-monitor'
         },
         {
-          name: "其他",
-          index: "5",
+          name: '其他',
+          index: '5',
           subNav: [
             {
-              name: "系统配置",
-              index: "5-1",
-              componentName: "ConfigurationSystem"
+              name: '系统配置',
+              index: '5-1',
+              componentName: 'ConfigurationSystem'
             },
             {
-              name: "模板设计器",
-              index: "5-2",
-              componentName: "TemplateDesigner"
+              name: '模板设计器',
+              index: '5-2',
+              componentName: 'TemplateDesigner'
             },
             {
-              name: "模板展示",
-              index: "5-3",
-              componentName: "TemplateDisplayer"
+              name: '模板展示',
+              index: '5-3',
+              componentName: 'TemplateDisplayer'
             },
-            { name: "修改密码", index: "5-4", componentName: "ChangePass" },
-            { name: "关于", index: "5-5", componentName: "About" }
+            { name: '修改密码', index: '5-4', componentName: 'ChangePass' },
+            { name: '关于', index: '5-5', componentName: 'About' }
           ],
-          icon: "el-icon-s-tools"
+          icon: 'el-icon-s-tools'
         }
       ],
       overviewList: [],
       activeIndex: null,
-      path: ""
-    };
+      path: ''
+    }
   },
   components: {
     // Overview
@@ -197,71 +222,71 @@ export default {
     // Hemodynamics
   },
   computed: {
-    ...mapGetters("Anaes", ["eventType"]),
-    oddEven(index) {
-      return function(index) {
+    ...mapGetters('Anaes', ['eventType']),
+    oddEven (index) {
+      return function (index) {
         if ((index + 1) % 2 === 0) {
-          return true;
+          return true
         } else {
-          return false;
+          return false
         }
-      };
+      }
     }
   },
   methods: {
-    ...mapActions("Anaes", ["setEventType"]),
-    jumpLogin() {
-      this.$router.push("/login");
+    ...mapActions('Anaes', ['setEventType']),
+    jumpLogin () {
+      this.$router.push('/login')
     },
-    handleDialogClose() {
-      this.showDialog = false;
-      this.activeIndex = false;
+    handleDialogClose () {
+      this.showDialog = false
+      this.activeIndex = false
     },
-    handleClose(done) {
-      done();
+    handleClose (done) {
+      done()
     },
-    handleChangeButton(item, index) {
-      this.activeIndex = index || null;
-      if (item.componentName === "Event") {
-        console.log("触发");
-        this.setEventType(item.index);
+    handleChangeButton (item, index) {
+      this.activeIndex = index || null
+      if (item.componentName === 'Event') {
+        console.log('触发')
+        this.setEventType(item.index)
       }
-      this.showDialog = true;
-      this.dialogTitle = item.name;
-      this.componentName = item.componentName;
+      this.showDialog = true
+      this.dialogTitle = item.name
+      this.componentName = item.componentName
     },
-    handleShowOverview() {
+    handleShowOverview () {
       if (this.isCollapse === true) {
-        this.showOverflow = !this.showOverflow;
+        this.showOverflow = !this.showOverflow
       }
     },
-    handleChange(active) {
-      this.activeIndex = null;
-      this.activesNames = active;
+    handleChange (active) {
+      this.activeIndex = null
+      this.activesNames = active
     },
-    handleOpen(key, keyPath) {
+    handleOpen (key, keyPath) {
       // console.log(key, keyPath)
     },
-    handleCloseMenu() {
+    handleCloseMenu () {
       if (this.showOverflow === false) {
-        this.isCollapse = !this.isCollapse;
+        this.isCollapse = !this.isCollapse
       }
     },
-    openConfiguration(route, name) {
-      this.$electron.ipcRenderer.send("open-new-window", route, name);
+    openConfiguration (route, name) {
+      this.$electron.ipcRenderer.send('open-new-window', route, name)
     }
   },
-  mounted() {
-    this.$eventHub.$on("show-dialog", item => {
+  mounted () {
+    this.$eventHub.$on('show-dialog', item => {
       // 激活弹窗
-      this.handleChangeButton(item);
-    });
-    this.$eventHub.$on("close-dialog", () => {
+      this.handleChangeButton(item)
+    })
+    this.$eventHub.$on('close-dialog', () => {
       // 关闭弹窗
-      this.handleDialogClose();
-    });
+      this.handleDialogClose()
+    })
   }
-};
+}
 </script>
 <style lang="scss" scoped>
 .aside {
