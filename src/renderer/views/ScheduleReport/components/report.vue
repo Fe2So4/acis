adaa51a009213b02bb96f9691e8598eead3a53d6
dyw-design <1875742887@qq.com>
2020-08-31 14:38:34 +0800

    fx

---
+++ src/renderer/views/ScheduleReport/components/report.vue
@@ -0,0 +1,209 @@
+<template>
+  <div class="schedule-report">
+    <table
+      width="100%"
+      cellspacing="0"
+      cellpadding="0"
+      align="center"
+      style="color:#444444;font-size:14px;"
+    >
+      <thead style="display: table-header-group;font-weight:bold">
+        <tr>
+          <th
+            colspan="14"
+            style="text-align:center;font-size:30px;padding:16px 0;border:0;"
+          >
+            手术排班表
+          </th>
+        </tr>
+        <tr>
+          <th
+            colspan="7"
+            style="text-align:left;padding:9px 0;"
+          >
+            时间：2020-8-31
+          </th>
+          <th
+            colspan="7"
+            style="text-align:right;padding:9px 0;"
+          >
+            台数：0台
+          </th>
+        </tr>
+        <tr>
+          <th style="border-top:1px solid #444444;border-right:1px solid #444444;border-left:1px solid #444444;padding:9px 0;">
+            术间
+          </th>
+          <th style="border-top:1px solid #444444;border-right:1px solid #444444;padding:9px 0;">
+            序号
+          </th>
+          <th style="border-top:1px solid #444444;border-right:1px solid #444444;padding:9px 0;">
+            病人信息
+          </th>
+          <th style="border-top:1px solid #444444;border-right:1px solid #444444;padding:9px 0;">
+            病区
+          </th>
+          <th style="border-top:1px solid #444444;border-right:1px solid #444444;padding:9px 0;">
+            床号
+          </th>
+          <th style="border-top:1px solid #444444;border-right:1px solid #444444;padding:9px 0;">
+            住院号
+          </th>
+          <th style="border-top:1px solid #444444;border-right:1px solid #444444;padding:9px 0;">
+            诊断
+          </th>
+          <th style="border-top:1px solid #444444;border-right:1px solid #444444;padding:9px 0;">
+            手术名称
+          </th>
+          <th style="border-top:1px solid #444444;border-right:1px solid #444444;padding:9px 0;">
+            手术医师
+          </th>
+          <th style="border-top:1px solid #444444;border-right:1px solid #444444;padding:9px 0;">
+            麻醉方法
+          </th>
+          <th style="border-top:1px solid #444444;border-right:1px solid #444444;padding:9px 0;">
+            麻醉医师
+          </th>
+          <th style="border-top:1px solid #444444;border-right:1px solid #444444;padding:9px 0;">
+            洗手护士
+          </th>
+          <th style="border-top:1px solid #444444;border-right:1px solid #444444;padding:9px 0;">
+            巡回护士
+          </th>
+          <th style="border-top:1px solid #444444;border-right:1px solid #444444;padding:9px 0;">
+            备注
+          </th>
+        </tr>
+      </thead>
+      <tbody style="text-align:center;">
+        <tr>
+          <td style="border-top:1px solid #444444;border-right:1px solid #444444;padding:9px 0;">
+            122
+          </td>
+          <td style="border-top:1px solid #444444;border-right:1px solid #444444;padding:9px 0;">
+            122
+          </td>
+          <td style="border-top:1px solid #444444;border-right:1px solid #444444;padding:9px 0;">
+            122
+          </td>
+          <td style="border-top:1px solid #444444;border-right:1px solid #444444;padding:9px 0;">
+            122
+          </td>
+          <td style="border-top:1px solid #444444;border-right:1px solid #444444;padding:9px 0;">
+            122
+          </td>
+          <td style="border-top:1px solid #444444;border-right:1px solid #444444;padding:9px 0;">
+            122
+          </td>
+          <td style="border-top:1px solid #444444;border-right:1px solid #444444;padding:9px 0;">
+            1221
+          </td>
+          <td style="border-top:1px solid #444444;border-right:1px solid #444444;padding:9px 0;">
+            2121
+          </td>
+          <td style="border-top:1px solid #444444;border-right:1px solid #444444;padding:9px 0;">
+            1221
+          </td>
+          <td style="border-top:1px solid #444444;border-right:1px solid #444444;padding:9px 0;">
+            2121
+          </td>
+          <td style="border-top:1px solid #444444;border-right:1px solid #444444;padding:9px 0;">
+            2121
+          </td>
+          <td style="border-top:1px solid #444444;border-right:1px solid #444444;padding:9px 0;">
+            2121
+          </td>
+          <td style="border-top:1px solid #444444;border-right:1px solid #444444;padding:9px 0;">
+            2121
+          </td>
+          <td style="border-top:1px solid #444444;border-right:1px solid #444444;padding:9px 0;">
+            2121
+          </td>
+        </tr>
+        <tr>
+          <td style="border-top:1px solid #444444;border-right:1px solid #444444;padding:9px 0;">
+            122
+          </td>
+          <td style="border-top:1px solid #444444;border-right:1px solid #444444;padding:9px 0;">
+            122
+          </td>
+          <td style="border-top:1px solid #444444;border-right:1px solid #444444;padding:9px 0;">
+            122
+          </td>
+          <td style="border-top:1px solid #444444;border-right:1px solid #444444;padding:9px 0;">
+            122
+          </td>
+          <td style="border-top:1px solid #444444;border-right:1px solid #444444;padding:9px 0;">
+            122
+          </td>
+          <td style="border-top:1px solid #444444;border-right:1px solid #444444;padding:9px 0;">
+            122
+          </td>
+          <td style="border-top:1px solid #444444;border-right:1px solid #444444;padding:9px 0;">
+            1221
+          </td>
+          <td style="border-top:1px solid #444444;border-right:1px solid #444444;padding:9px 0;">
+            2121
+          </td>
+          <td style="border-top:1px solid #444444;border-right:1px solid #444444;padding:9px 0;">
+            1221
+          </td>
+          <td style="border-top:1px solid #444444;border-right:1px solid #444444;padding:9px 0;">
+            2121
+          </td>
+          <td style="border-top:1px solid #444444;border-right:1px solid #444444;padding:9px 0;">
+            2121
+          </td>
+          <td style="border-top:1px solid #444444;border-right:1px solid #444444;padding:9px 0;">
+            2121
+          </td>
+          <td style="border-top:1px solid #444444;border-right:1px solid #444444;padding:9px 0;">
+            2121
+          </td>
+          <td style="border-top:1px solid #444444;border-right:1px solid #444444;padding:9px 0;">
+            2121
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+<script>
+export default {
+  name: 'ScheduleReport',
+  data () {
+    return {
+      tableData: []
+    }
+  },
+  computed: {},
+  methods: {
+  },
+  mounted () {
+  }
+}
+</script>
+<style lang="scss" scoped>
+  .schedule-report{
+    height: 100%;
+    // background: #E3E3E3;
+    // box-shadow: 1px 20px 45px 5px rgba(0, 0, 0, 0.5);
+    // border-radius: 5px;
+    table{
+      tbody{
+        tr{
+          td{
+            &:first-child{
+              border-left: 1px solid #444444;
+            }
+          }
+          &:last-child{
+            td{
+              border-bottom:1px solid #444444;
+            }
+          }
+        }
+      }
+    }
+  }
+</style>
