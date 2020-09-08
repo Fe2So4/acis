562409b81176c2477dc25991f3957dba90828d63
weifeng.zhang <wulakaka@live.com>
2020-09-08 16:49:26 +0800

    增加echarts图主题色调整

--- src/renderer/views/AnaesScore/components/charts.vue
+++ src/renderer/views/AnaesScore/components/charts.vue
@@ -1,47 +1,88 @@
 <template lang="pug">
-  .score-chart
-    .chart(ref="chart")
-    .table
-      vxe-table(
-        border
-        show-header-overflow
-        show-overflow
-        highlight-hover-row
-        align="center"
-        height="100%"
-        auto-resize
-        size="mini"
-        class="scroll"
-        :data="tableData"
-      )
-        vxe-table-column(field="name" title="评估时间")
-        vxe-table-column(field="specs" title="评估结果")
-        vxe-table-column(field="specs" title="评估人")
+.score-chart
+  .chart(ref="chart")
+  .table
+    vxe-table.scroll(
+      border,
+      show-header-overflow,
+      show-overflow,
+      highlight-hover-row,
+      align="center",
+      height="100%",
+      auto-resize,
+      size="mini",
+      :data="tableData"
+    )
+      vxe-table-column(field="name", title="评估时间")
+      vxe-table-column(field="specs", title="评估结果")
+      vxe-table-column(field="specs", title="评估人")
 </template>
 <script>
 import echarts from 'echarts'
+import { mapState } from 'vuex'
 export default {
   name: 'ScoreChart',
   data () {
     return {
-      tableData: []
+      tableData: [],
+      chart: null
+    }
+  },
+  computed: {
+    ...mapState('Base', ['theme']),
+    textColor () {
+      switch (this.theme) {
+        case 'dark-blue':
+          return '#9BA3D5'
+        case 'dark-gray':
+          return '#BABABA'
+        case 'light-white':
+          return '#707C91'
+        default:
+          return ''
+      }
+    },
+    axisLineColor () {
+      switch (this.theme) {
+        case 'dark-blue':
+          return '#626D8F'
+        case 'dark-gray':
+          return '#BABABA'
+        case 'light-white':
+          return '#CECECE'
+        default:
+          return ''
+      }
+    },
+    splitLineColor () {
+      switch (this.theme) {
+        case 'dark-blue':
+          return '#32394B'
+        case 'dark-gray':
+          return '#5D5D5F'
+        case 'light-white':
+          return '#EBEBEB'
+        default:
+          return ''
+      }
     }
   },
   methods: {
     createCharts () {
       // 基于准备好的dom，初始化echarts实例
-      var myChart = echarts.init(this.$refs.chart)
+      this.chart = echarts.init(this.$refs.chart)
       // 绘制图表
       const option = {
         xAxis: {
           type: 'category',
           // data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-          axisTick: { // 去掉坐标轴刻线
+          axisTick: {
+            // 去掉坐标轴刻线
             show: false
           },
           axisLine: {
             lineStyle: {
-              color: '#616D8F'
+              color: this.axisLineColor
             }
           }
         },
@@ -53,17 +94,18 @@ export default {
         },
         yAxis: {
           type: 'value',
-          axisTick: { // 去掉坐标轴刻线
+          axisTick: {
+            // 去掉坐标轴刻线
             show: false
           },
           axisLine: {
             lineStyle: {
-              color: '#616D8F'
+              color: this.axisLineColor
             }
           },
           splitLine: {
             lineStyle: {
-              color: ['#333'],
+              color: [this.splitLineColor],
               width: 1,
               type: 'solid'
             }
@@ -76,38 +118,54 @@ export default {
             { time: '2020-7-16', value: '18' }
           ]
         },
-        series: [{
-          // data: [10, 20, 9, 10, 12, 13, 10],
-          type: 'line',
-          lineStyle: {
-            // color: '#616D8F'
+        series: [
+          {
+            // data: [10, 20, 9, 10, 12, 13, 10],
+            type: 'line',
+            lineStyle: {
+              // color: '#616D8F'
+            }
           }
-        }],
+        ],
         tooltip: {
           trigger: 'axis'
         },
         textStyle: {
-          color: '#9BA3D5'
+          color: this.textColor
         }
       }
-      myChart.setOption(option)
+      this.chart.setOption(option)
     }
   },
   mounted () {
     this.createCharts()
+  },
+  beforeDestroy () {
+    this.chart.dispose()
   }
 }
 </script>
-<style lang="stylus" scoped>
-  .score-chart
-    height 200px
-    display flex
-    background #1E222E
-    justify-content space-between
-    .table
-      width 506px
-      height 200px
-    .chart
-      height 200px
-      width 332px
+<style lang="scss" scoped>
+@import "@/styles/theme";
+$background: (
+  "dark-blue": #1e222e,
+  "dark-gray": #363638,
+  light-white: #f8f8f8,
+);
+.score-chart {
+  height: 200px;
+  display: flex;
+  @include theme-property("background", $background);
+  justify-content: space-between;
+
+  .table {
+    width: 506px;
+    height: 200px;
+  }
+
+  .chart {
+    height: 200px;
+    width: 332px;
+  }
+}
 </style>
