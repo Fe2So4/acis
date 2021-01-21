bd0425166a597df10ca789e9be8ba4e21f80d0ff
weifeng.zhang <wulakaka@live.com>
2021-01-21 15:02:15 +0800

    logo文件调整
    将logo改为蓝想

--- src/renderer/views/HomeNew/Aside/index.vue
+++ src/renderer/views/HomeNew/Aside/index.vue
@@ -328,7 +328,7 @@ export default {
       }
     },
     logoSource () {
-      return require(`@/assets/fj_${this.theme}.png`)
+      return require(`@/assets/lan_${this.theme}.png`)
     },
     menuBackground () {
       switch (this.theme) {
